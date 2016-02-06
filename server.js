9b36d3546dc301a1e9209155b7e90f066c8fdaf2
Eli Semory <elisemory@gmail.com>
2016-02-06 01:56:29 -0500

    WIP

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 var AlexaAppServer = require('alexa-app-server');
 var port = process.env.PORT || 3000
 
-AlexaAppServer.start({
-	//httpsPort:4000,
-	port:port,
-	//httpsEnabled:true,
-	//privateKey:'private-key.pem',
-	//certificate:'cert.cer'
+var appServer = AlexaAppServer.start({
+		port:port
 	}
 );
