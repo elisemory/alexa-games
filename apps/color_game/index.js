df212022fab7cf87e854116818dcfdae65cac058
Eli Semory <elisemory@gmail.com>
2016-02-06 21:47:00 -0500

    wip

--- apps/color_game/index.js
+++ apps/color_game/index.js
@@ -1,10 +1,12 @@
 var alexa = require('alexa-app');
 
+var io;
+
 // Allow this module to be reloaded by hotswap when changed
 module.change_code = 1;
 
 // Define an alexa-app
-var app = new alexa.app('helloworld');
+var app = new alexa.app('colorgame');
 app.launch(function(req,res) {
 	res.say("Hello World!!").shouldEndSession(false);
 });
@@ -25,6 +27,15 @@ app.intent('AgeIntent', {
 
 app.game = function(req, res) {
 	res.render('index');
+}
+
+app.io = function(server_io) {
+	console.log(app.name + ' setio');
+	io = server_io;
+	io.on('connection', function(socket){
+			console.log(app.name + ' io connect');
+			io.emit('chat message', app.name);
+  	})
 }
 
 module.exports = app;
