71679d7f0abc018c8882829a3637f173143dca15
Eli Semory <elisemory@gmail.com>
2016-02-07 01:11:33 -0500

    wip

--- apps/color_game/index.js
+++ apps/color_game/index.js
@@ -7,7 +7,7 @@ module.change_code = 1;
 // Define an alexa-app
 var app = new alexa.app('colorgame');
 app.launch(function(req,res) {
-	res.say('Welcome! <audio src="https://s3.amazonaws.com/elasticbeanstalk-us-east-1-038897603251/alexa-games/test.mp3" />').shouldEndSession(false);
+	res.say('Welcome! <audio src="https://alexa-games-dev.elasticbeanstalk.com/alexa/colorgame/media/?id=123" />').shouldEndSession(false);
 });
 
 var colorsArr = ["red","yellow","green","white","blue","black"];
@@ -22,6 +22,11 @@ app.intent('ColorIntent', {
 
 app.game = function(req, res) {
 	res.render('index');
+}
+
+app.media = function(req, res) {
+	console.log('@@@ media ' + req.query.id);
+	res.sendFile('test.mp3', {root: './apps/color_game/media'});
 }
 
 app.io = function(server_io) {
